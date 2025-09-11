46095d9f82ed24edd0cede6f0f17bf7ef2f03809
isita <isita@banregio.com>
2025-09-11 10:51:16 -0600

    primer commit repositorio principal

---
+++ src/main/java/com/iep/mycompany/app/TemplateBackendFrontApplication.java
@@ -0,0 +1,13 @@
+package com.iep.mycompany.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBackendFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBackendFrontApplication.class, args);
+	}
+
+}
